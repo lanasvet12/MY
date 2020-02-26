899ee5027526d08d6d6f1f1dc2cb7bb478f41450
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-02-26 18:20:24 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-from app import app
+from flask import Flask, render_template      
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+  We made two new changes
