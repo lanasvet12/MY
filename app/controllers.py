58b2346b26f63e4969efa461e522068bee193c8d
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-02-27 15:12:06 +0200

    Update controllers.py

--- app/controllers.py
+++ app/controllers.py
@@ -3,6 +3,6 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello World!"
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/about)
+def about():
+    return render_template("about.html")
